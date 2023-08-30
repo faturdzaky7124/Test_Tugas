fd6a4ed9a401548f7d8403db0a7c5ef5a483f43d
faturdzaky7124 <faturrahmandzaky2@gmail.com>
2023-08-30 21:35:57 +0700

    Fix First Upload

---
+++ Untitled-1.php
@@ -0,0 +1,10 @@
+<?php
+//Menampilkan Data Diri
+echo "Nama: Mochammad Faturrahman Dzaky
+      NIM: E41220669"
+//Tugas penjumlahan sederhana
+$a =7;
+$b =3;
+$hasil = $a + $b;
+echo "hasil dari $a + $b = $hasil";
+?>
